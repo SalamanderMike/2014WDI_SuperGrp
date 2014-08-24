ae366a84d073735f00f1bf0a03e51019dab40ca2
maiahariton <maia.hariton@gmail.com>
2014-08-24 13:01:21 -0700

    last tag validation

--- micro_blog/app/models/tag.rb
+++ micro_blog/app/models/tag.rb
@@ -13,15 +13,17 @@ class Tag < ActiveRecord::Base
  			maximum: 4,
  			too_short: "must have at least %{count} words",
     	too_long: "%{count} characters is the maximum allowed" }, presence: true
-    	tokenizer: lambda { |str| str.scan(/\w+/) }
+    	tokenizer: lambda { |str| str.scan(/\w+/) },
   }
-  validates format: { with: /^\s*[a-zA-Z0-9]+\s*$/,
-    				message: "The tag cannot have any spaces or special characters"
+  validates
+  format: { with: /^\s*[a-zA-Z0-9]+\s*$/,
+  message: "The tag cannot have any spaces or special characters",
   }
-
 
 
 
 	has_many :post_tags
 	has_many :posts, :through => :post_tags
 end
+
+
