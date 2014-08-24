f6a8f2d2b25d5e3c6476c15b92cd2c2a3de333ab
ncaskey04 <ncaskey04@gmail.com>
2014-08-23 19:23:00 -0700

    completed model attribute tests

--- micro_blog/spec/models/comment_spec.rb
+++ micro_blog/spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has necessary attributes" do
+    comment = Comment.new(content: "I love bananas...alot!", post_id: 1)
+    expect(comment.content).to eq("I love bananas...alot!")
+    expect(comment.post_id).to eq(1)
+  end
+
 end
--- micro_blog/spec/models/post_tag_spec.rb
+++ micro_blog/spec/models/post_tag_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostTag, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has necessary attributes" do
+    pt = PostTag.new(post_id: 1, tag_id: 1)
+    expect(pt.post_id).to eq(1)
+    expect(pt.tag_id).to eq(1)
+  end
+  
 end
