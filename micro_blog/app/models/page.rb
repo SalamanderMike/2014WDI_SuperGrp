75f5e89ff56262b2408a5ec10cc79fa5c153ee6b
SanaNasar <sanasehereen.cse@gmail.com>
2014-08-23 19:35:34 -0700

    Adding validations to page model

--- micro_blog/app/models/page.rb
+++ micro_blog/app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ActiveRecord::Base
+	# validation to verify name, content
+ 	validates :name, presence: true
+ 	validates :content, presence: true, length: {
+    minimum: 300,
+    maximum: 400,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words",
+    too_long: "must have at most %{count} words"
+  }
 	belongs_to :user
 end
