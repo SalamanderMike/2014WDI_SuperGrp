9c2d2115054d14a78d2875143dc364e924b2daae
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-08-24 13:46:54 -0700

    fixed validations on page and user model

--- micro_blog/app/models/page.rb
+++ micro_blog/app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
 	# validation to verify name, content
  	validates :name, presence: true
  	validates :content, presence: true, length: {
-    minimum: 300,
+    minimum: 1,
     maximum: 400,
     tokenizer: lambda { |str| str.scan(/\w+/) },
     too_short: "must have at least %{count} words",
