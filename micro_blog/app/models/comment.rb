9b24045a146b0cdd7d4381a61fc2a787d57be37e
maiahariton <maia.hariton@gmail.com>
2014-08-24 12:14:46 -0700

    validations everywhere and REGEX is working

--- micro_blog/app/models/comment.rb
+++ micro_blog/app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
 
+	#validation for comment verify content
+	# length min, max
+	# presence
+
+
+	 	validates :content, presence: true, length: {
+    minimum: 15,
+    maximum: 40,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words",
+    too_long: "must have at most %{count} words"
+  }
 
 	belongs_to :post
 	has_many :comments, as: :commentable
--- micro_blog/app/models/tag.rb
+++ micro_blog/app/models/tag.rb
@@ -1,4 +1,23 @@
 class Tag < ActiveRecord::Base
+	# validation for verify name
+		# uniqueness
+		# presence
+		# max and min length
+		# format
+		# no spaces or special characters
+
+
+	validates :name, uniqueness: true, presence: true
+ 	validates :length {
+    minimum: 1,
+    maximum: 4,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words",
+    too_long: "must have at most %{count} words"
+  }
+  validates format: { with: /^\s*[a-zA-Z0-9]+\s*$/,
+    message: "The tag cannot have any spaces or special characters"
+  }
 
 
 
